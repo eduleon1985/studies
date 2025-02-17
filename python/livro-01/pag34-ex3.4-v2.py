71eed6709b6856b98f6fa05a2c869acee6a27c97
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-02-17 10:01:37 -0300

    Added two Python practice scripts

---
+++ python/livro-01/pag34-ex3.4-v2.py
@@ -0,0 +1,8 @@
+# Escreva uma expressão para determinar  se uma pessoa deve ou não pagar imposto.
+# Considere que pagam imposto pessoas cujo salário seja maior que R$ 3.000,00
+
+salario = 3050
+
+paga_imposto = salario > 3000
+
+print(paga_imposto)
---
+++ python/livro-01/pag34-ex3.4.py
@@ -0,0 +1,8 @@
+# Escreva uma expressão para determinar  se uma pessoa deve ou não pagar imposto.
+# Considere que pagam imposto pessoas cujo salário seja maior que R$ 3.000,00
+
+salario = 3000
+if salario > 3000:
+    print("Give me your money, I'm the Government")
+else:
+    print("You're a loser!")
