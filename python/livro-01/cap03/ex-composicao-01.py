e4654d1d9f59c52ff6a8a4812765fb0ea925429a
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-02-20 14:28:04 -0300

    Added some string composition exercises usein f-strings for practice

---
+++ python/livro-01/cap03/ex-composicao-01.py
@@ -0,0 +1,13 @@
+# 1 - Saudação personalizada | Personalized Greeting
+# 📌 Português:
+# Peça ao usuário seu nome e o horário do dia (manhã, tarde ou noite).
+# Exiba a mensagem:
+# "Bom [período], [nome]! Espero que você tenha um ótimo dia!"
+# "Good [time of day], [name]! I hope you have a great day!"
+
+nome = "Edu"
+periodo = "bom dia"
+msg = f"Olá, {periodo}, {nome}! Seja bem-vindo!"
+print(msg)
+msg2 = f"Hello, {periodo}, {nome}! Welcome!"
+print(msg2)
---
+++ python/livro-01/cap03/ex-composicao-02.py
@@ -0,0 +1,16 @@
+# 2 - Informações sobre um produto | Product Information
+
+# 📌 Português:
+# Peça ao usuário o nome de um produto e seu preço.
+# Exiba a mensagem: "O produto [produto] custa R$ [preço] reais."
+
+# 📌 English:
+# Ask the user for a product name and its price.
+# Display the message: "The product [product] costs $[price]."
+
+produto = "iPhone"
+preco = 15000
+msg = f"O produto {produto} custa R${preco}"
+msg2 = f"The product {produto} costs R${preco}"
+print(msg)
+print(msg2)
