622da07b1e682067b5dee1f6fbef447fb7cfe76e
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-02-20 22:15:02 -0300

    Added a string slicing exercise for practice

---
+++ python/livro-01/cap03/ex-fatiamento-02.py
@@ -0,0 +1,9 @@
+# 2 - Exibindo os últimos 4 caracteres | Displaying the last 4 characters
+# 📌 Português:
+# Peça ao usuário uma palavra e exiba apenas os últimos 4 caracteres.
+
+# 📌 English:
+# Ask the user for a word and display only the last 4 characters.
+
+palavra = input("Digite uma palavra qualquer: ")
+print(palavra[-4:])
