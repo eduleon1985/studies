7c5d12e61ec2717e8c7df5816baccd04fbfe727b
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-02-21 23:24:26 -0300

    Added another string slicing exercise to practice

---
+++ python/livro-01/cap03/exercicios-complementares/ex-complementar-fatiamento04.py
@@ -0,0 +1,7 @@
+# Pegando os 3 primeiros caracteres de um nome
+# 📌 Português:
+# Peça ao usuário seu nome completo e exiba apenas os três primeiros caracteres.
+
+nome = input("Magnata, diga aí seu nome: ")
+iniciais = nome[:3]
+print(f"Seu nome completo é {nome} e as iniciais são {iniciais}")
