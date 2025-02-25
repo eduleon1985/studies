b639d9b0dfc39c078333f60901b3caa9382938ba
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-02-24 23:47:34 -0300

    Added an exercise for converting meters to milimeters

---
+++ python/livro-01/cap03/pag113-ex3.8.py
@@ -0,0 +1,6 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em milímetros.
+# Write a program that reads a value in meters and displays it converted to milimiters.
+
+x = int(input("Enter any integer value in meters: "))
+y = x * 1000
+print(f"The value in meters is {x} and in milimiters is {y}")
