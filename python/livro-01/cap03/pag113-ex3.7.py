085f424a126225102da8ab150e8d8b0274e5fb33
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-02-24 23:39:17 -0300

    Added an exerciste for converting user input to integers

---
+++ python/livro-01/cap03/pag113-ex3.7.py
@@ -0,0 +1,7 @@
+# PT-BR: Faça um programa que peça dois números inteiros. Imprima a soma destes dois números na tela.
+# EN: Write a program that asks for two integer numbers. Print the sum of these two numbers on the screen.
+
+num1 = int(input("Enter any integer number: "))
+num2 = int(input("Enter other integer number: "))
+sum = num1 + num2
+print(f"The sum of the two numbers is {sum}")
