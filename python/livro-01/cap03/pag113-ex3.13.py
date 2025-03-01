c03bb4dce5bdd45310d2436072dd54d27eab3e11
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-02-28 22:57:15 -0300

    Added a program to convert temperature from Celsius to Fahrenheit

---
+++ python/livro-01/cap03/pag113-ex3.13.py
@@ -0,0 +1,11 @@
+# PT-BR (🇧🇷): 
+# Escreva um programa que converta uma temperatura digitada em ºC para Fº.
+
+# EN (🇺🇸): 
+# Write a program that converts a temperature entered in ºC to ºF.
+celsius = float(input("Informe a temperatura em ºC: "))
+fahrenheit = ((9 * celsius)/5) + 32
+print("===")
+print(f"Temperatura em Celsius: {celsius}º")
+print(f"Temperatura em Fahrenheit: {fahrenheit}º")
+print("===")
