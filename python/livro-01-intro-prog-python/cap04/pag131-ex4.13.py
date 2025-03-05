8868b0fa36ccb2b1a1084693fc89897c5ba6ca33
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-03-05 17:12:45 -0300

    Inverted if-else conditions with logical negation

---
+++ python/livro-01-intro-prog-python/cap04/pag131-ex4.13.py
@@ -0,0 +1,19 @@
+### PT-BR (🇧🇷): 
+# Inverta as linhas do if e else, negando a condição.
+
+### EN (🇺🇸):  
+# Invert the if and else lines by negating the condition.
+
+### Programa original
+# if a > b:
+#   print("a > b")
+# else:
+#   print("b > a")
+
+a = True
+b = True
+
+if a <= b:
+    print("b > a")
+else:
+    print("a > b")
