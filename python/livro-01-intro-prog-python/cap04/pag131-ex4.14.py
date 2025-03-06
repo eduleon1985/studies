576361d7e2f4a4b30d005b3b04a8d4866defb96f
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-03-06 14:02:43 -0300

    Added if-elif-else strucutre in the program

---
+++ python/livro-01-intro-prog-python/cap04/pag131-ex4.14.py
@@ -0,0 +1,25 @@
+### PT-BR (🇧🇷): 
+# Reescreva o programa a seguir com if-elif-else.
+# Adicione as linhas necessárias para fazê-lo funcionar em Python.
+
+### EN (🇺🇸):  
+# Rewrite the following program using if-elif-else.  
+# Add the necessary lines to make it work in Python.
+
+'''
+if a < 10:
+    print("a < 10")
+if a >= 10 and a < 20:
+    print("a > 10 e a < 20")
+if a >= 20""
+    print("a > 20")
+'''
+
+a = int(input("Digite um valor para A: "))
+
+if a < 10:
+    print("A < 10")
+elif a >= 10 and a < 20:
+    print("A >= 10 e < 20")
+else:
+    print("A > 20")
