bb38426e8dcbc83128e34ecc5993fa14de3b7817
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-03-06 15:04:16 -0300

    Added two exercises using while loops with counters

---
+++ python/livro-01-intro-prog-python/cap05/pag140-ex5.5.py
@@ -0,0 +1,10 @@
+### PT-BR (🇧🇷): 
+# Crie um programa que escreva os 10 primeiros múltiplos de 3.
+
+### EN (🇺🇸):  
+# Create a program that asks fo
+
+i = 1
+while i <= 10:
+    print(i * 3)
+    i = i + 1
