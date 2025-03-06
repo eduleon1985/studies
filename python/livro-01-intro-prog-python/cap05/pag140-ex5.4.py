dc06576f8c60c690ff85de43ca54319021089e94
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-03-06 14:54:17 -0300

    Added a while loop counter for odd numbers.

---
+++ python/livro-01-intro-prog-python/cap05/pag140-ex5.4.py
@@ -0,0 +1,11 @@
+### PT-BR (🇧🇷): 
+# Crie um programa que solicite um número inteiro positivo e exiba todos os números ímpares de 1 até esse número, utilizando a estrutura while e um contador.
+
+### EN (🇺🇸):  
+# Create a program that asks for a positive integer and displays all odd numbers from 1 up to that number, using the while loop and a counter.
+
+fim = int(input("Informe um valor para X: "))
+x = 1
+while (x <= fim):
+    print(x)
+    x = x + 2
