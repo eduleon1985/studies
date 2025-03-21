69345e62cc79da70b4b13eacc9becf0b88af14e3
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-03-20 23:55:41 -0300

    Implemented a program to sum values from a list using a for loop

---
+++ python/livro-01-intro-prog-python/cap06/exercicio02-for.py
@@ -0,0 +1,16 @@
+### PT-BR (🇧🇷): 
+# Crie uma lista com 5 números.
+# Use um for para somar todos os valores da lista e exibir o total.
+
+### EN (🇺🇸):
+# Create a list with 5 numbers.
+# Use a for loop to sum all the values in the list and display the total.
+
+lista = [10, 20, 30, 40, 50]
+
+soma = 0
+
+for numero in lista:
+    soma += numero
+
+print(f"A soma da lista {lista} é {soma}")
