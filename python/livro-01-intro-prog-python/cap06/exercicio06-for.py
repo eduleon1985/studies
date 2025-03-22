6be3261864850a6df3c73b5647f724ad19d11aed
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-03-22 19:44:48 -0300

    Added a program that lists 5 of my favorite movies using a for loop

---
+++ python/livro-01-intro-prog-python/cap06/exercicio06-for.py
@@ -0,0 +1,12 @@
+### PT-BR (🇧🇷): 
+# Crie uma lista com 5 filmes que você gosta.
+# Use um laço for para exibir a mensagem: "Eu adoro o filme [filme]!"
+
+### EN (🇺🇸):
+# Create a list with 5 movies you like.
+# Use a for loop to display the message: "I love the movie [movie]!"
+
+filmes = ["A felicidade não se compra", "Um sonho de liberdade", "A vida é bela", "Milagre da cela 7", "Tropa de Elite"]
+
+for i in filmes:
+    print(f"{i} é um dos meus filmes preferidos!")
