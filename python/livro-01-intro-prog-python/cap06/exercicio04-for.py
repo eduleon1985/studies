dc5a4ce926f12013146b20953299535b491db191
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-03-22 19:38:15 -0300

    Added a program that lists fruits and confirms each was added to the cart using a for loop

---
+++ python/livro-01-intro-prog-python/cap06/exercicio04-for.py
@@ -0,0 +1,12 @@
+### PT-BR (🇧🇷): 
+# Crie uma lista com o nome de 7 frutas.
+# Use um for para exibir uma mensagem dizendo que cada fruta foi adicionada ao carrinho.
+
+### EN (🇺🇸):
+# Create a list with the names of 7 fruits.  
+# Use a for loop to display a message saying that each fruit was added to the cart.
+
+frutas = ["Banana", "Pêra", "Morango", "Uva", "Abacaxi", "Laranja", "Kiwi"]
+
+for i in frutas:
+    print(f"{i} adicionada ao carrinho com sucesso!")
