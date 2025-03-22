2f3bbec6c16e0876550bdb88ef82ffae1133fed2
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-03-22 19:42:24 -0300

    Added a program that prints a travel wish list using a for loop

---
+++ python/livro-01-intro-prog-python/cap06/exercicio05-for.py
@@ -0,0 +1,12 @@
+### PT-BR (🇧🇷): 
+# Crie uma lista com 6 nomes de cidades que você gostaria de visitar.
+# Use um laço for para exibir a frase: "Quero muito conhecer [cidade]!"
+
+### EN (🇺🇸):
+# Create a list with 6 city names you'd like to visit.
+# Use a for loop to display the message: "I really want to visit [city]!"
+
+cidades = ["Gramado", "Parati", "Cabo Frio", "São Miguel d'Oeste", "Urubici", "Jaú"]
+
+for i in cidades:
+    print(f"Gostaria muito de conhecer {i}")
