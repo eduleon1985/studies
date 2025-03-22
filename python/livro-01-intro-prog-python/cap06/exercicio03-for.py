62e45e6b54d3ed7c0556d8ddc49a20ae7a9ea356
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-03-21 23:01:29 -0300

    Added a program that greets each name in a list using a for loop

---
+++ python/livro-01-intro-prog-python/cap06/exercicio03-for.py
@@ -0,0 +1,12 @@
+### PT-BR (🇧🇷): 
+# Crie um programa que percorra uma lista de nomes usando um laço for.
+# Para cada nome na lista, exiba a mensagem: "Olá, [nome]!"
+
+### EN (🇺🇸):
+# Create a program that goes through a list of names using a for loop.
+# For each name in the list, display the message: "Hello, [name]!"
+
+nomes = ["Edu", "Carol", "Rafa", "Mari", "Gui"]
+
+for i in nomes:
+    print(f"Olá, {i}")
