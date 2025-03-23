b5f63dc4d215bc28e1d247e46008c47d48ef6fea
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-03-23 15:14:07 -0300

    Added a program that prints even numbers from 2 to 20 using range

---
+++ python/livro-01-intro-prog-python/cap06/exercicio13-range.py
@@ -0,0 +1,10 @@
+### PT-BR (🇧🇷): 
+# Crie um programa que exiba todos os números pares de 2 até 20 usando a função range.
+# Para cada número, exiba a mensagem: "[número] é par."
+
+### EN (🇺🇸):
+# Create a program that displays all even numbers from 2 to 20 using the range function.
+# For each number, display the message: "[number] is even."
+
+for i in range(2, 21, 2):
+    print(f"{i} é par.")
