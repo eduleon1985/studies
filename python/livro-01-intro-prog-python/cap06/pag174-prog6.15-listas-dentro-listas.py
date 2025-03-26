a944b178fad39b2bc2ae60cc198fe42e97b5ec13
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-03-25 23:59:10 -0300

    Added a program that accesses characters from strings inside nested lists

---
+++ python/livro-01-intro-prog-python/cap06/pag174-prog6.15-listas-dentro-listas.py
@@ -0,0 +1,8 @@
+frutas = ["Maçã", "Pêra", "Banana", "Morango", "Kiwi"]
+print(frutas[0][0])
+print(frutas[0][1])
+print(frutas[0][2])
+print(frutas[0][3])
+print(frutas[1][0])
+print(frutas[1][1])
+print(frutas[2][2])
