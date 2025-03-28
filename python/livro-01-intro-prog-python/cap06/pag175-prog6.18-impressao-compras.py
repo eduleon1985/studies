a4bbe3c5b0801ca9d27c5d8ffcc03577420a61a2
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-03-27 23:56:08 -0300

    For each element, it's own list

---
+++ python/livro-01-intro-prog-python/cap06/pag175-prog6.18-impressao-compras.py
@@ -0,0 +1,11 @@
+produto1 = ["Maçã", 10, "0.30"]
+produto2 = ["Pêra", 5, "0.75"]
+produto3 = ["Kiwi", 4, "0.98"]
+
+compras = [produto1, produto2, produto3]
+
+for e in compras:
+    print(f"Produto: {e[0]}")
+    print(f"Produto: {e[1]}")
+    print(f"Produto: {e[2]}")
+    print("*" * 10)
