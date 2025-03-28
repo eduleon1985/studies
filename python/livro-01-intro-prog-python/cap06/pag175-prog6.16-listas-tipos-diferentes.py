2bb3f0ed161be878d826347be2d437fe5829b8eb
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-03-27 23:52:07 -0300

    List with different data types

---
+++ python/livro-01-intro-prog-python/cap06/pag175-prog6.16-listas-tipos-diferentes.py
@@ -0,0 +1,6 @@
+produto1 = ["Maçã", 10, "0.30"]
+produto2 = ["Pêra", 5, "0.75"]
+produto3 = ["Kiwi", 4, "0.98"]
+print(f"Produto 1: {produto1}")
+print(f"Produto 2: {produto2}")
+print(f"Produto 3: {produto3}")
