d26bf5062197c0e17bb298b491f861f007e880da
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-04-01 00:44:15 -0300

    Dictionary exercise using keys and values functions

---
+++ python/livro-01-intro-prog-python/cap06/exercises/exercicio26-dicionario-keys-values.py
@@ -0,0 +1,18 @@
+tabela = {
+    "Alface" : 0.45,
+    "Cenoura" : 0.33,
+    "Pimentão" : 1.27,
+    "Banana" : 0.69,
+    "Laranja" : 0.73,
+    "Maçã" : 2.30,
+    "Pêra" : 2.59,
+    "Melancia" : 0.23,
+    "Maracujá" : 4.99
+}
+
+
+# Imprime apenas as chaves do dicionário
+print(tabela.keys())
+
+# Imprime apenas os valores do dicionário
+print(tabela.values())
