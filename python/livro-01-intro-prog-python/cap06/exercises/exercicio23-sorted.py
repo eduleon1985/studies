1e76011ca7e4628609f8cfacf3e6e6c728df77da
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-04-01 00:22:48 -0300

    Dictionary exercise

--- python/livro-01-intro-prog-python/cap06/exercises/exercicio23-sorted.py
+++ python/livro-01-intro-prog-python/cap06/exercises/exercicio23-sorted.py
@@ -1,5 +1,4 @@
-L = [6, 35, 63, 123, 6324, 634, 324, 65, 672, 463]
-L.sort()
-print(L)
-L.sorted()
-print(L)
+numeros = [4, 3, 64, 63, 1, 7, 23]
+nova_lista = sorted(numeros)
+print(numeros)
+print(nova_lista)
---
+++ python/livro-01-intro-prog-python/cap06/exercises/exercicio24-dicionario-beatles.py
@@ -0,0 +1,14 @@
+beatles = {
+    "John" : "guitarra base",
+    "George" : "guitarra solo",
+    "Paul" : "baixo",
+    "Ringo" : "bateria"
+}
+
+print(beatles)
+print("=" * 30)
+
+print(beatles["John"])
+print(beatles["George"])
+print(beatles["Paul"])
+print(beatles["Ringo"])
