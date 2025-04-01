60197e664366f9aa3957a16d4eb85872780fc373
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-04-01 00:25:39 -0300

    Dictionary exercise - part 2 - missing item in dictionary

---
+++ python/livro-01-intro-prog-python/cap06/exercises/exercicio25-dicionario-beatles2.py
@@ -0,0 +1,17 @@
+beatles = {
+    "John" : "guitarra base",
+    "George" : "guitarra solo",
+    "Paul" : "baixo",
+    "Ringo" : "bateria"
+}
+
+print(beatles)
+print("=" * 30)
+
+print(beatles["John"])
+print(beatles["George"])
+print(beatles["Paul"])
+print(beatles["Ringo"])
+print("-" * 30)
+print("John" in beatles)
+print("Freddie Mercury" in beatles)
