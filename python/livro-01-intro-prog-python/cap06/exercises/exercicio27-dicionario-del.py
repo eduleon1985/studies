c74a400efd22be0be424de766561c87c7fb226da
Luis Eduardo Leon <33657858+eduleon1985@users.noreply.github.com>
2025-04-03 23:32:41 -0300

    Added a script that deletes a key from a dictionary using del instruction

---
+++ python/livro-01-intro-prog-python/cap06/exercises/exercicio27-dicionario-del.py
@@ -0,0 +1,12 @@
+veiculos = {
+    "honda": "zrv",
+    "fiat": "toro",
+    "chevrolet": "onix",
+    "toyota": "corolla",
+    "ford": "del rey",
+    "hyunday": "hb20"
+}
+
+print(veiculos)
+del veiculos["hyunday"]
+print(veiculos)
